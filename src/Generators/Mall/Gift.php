7fb2c35d750cccf9db4bdb8869106c9953a5f7aa
Petr Vojacek <pecan987@gmail.com>
2021-11-01 00:42:47 +0100

    Add Mall generator

---
+++ src/Generators/Mall/Gift.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Mk\Feed\Generators\Mall;
+
+use Mk, Nette;
+
+/**
+ * Class Gift
+ * @author Martin Knor <martin.knor@gmail.com>
+ * @package Mk\Feed\Generators\Heureka
+ */
+class Gift {
+
+    /* Použití smartobject viz php 7.2 to nette 2.4 */
+    use \Nette\SmartObject;
+
+    /** @var string */
+    protected $name;
+
+    /** @var int */
+    protected $id;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+	/**
+	 * @return int
+	 */
+	public function getId(): int
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Gift constructor.
+	 * @param $name
+	 * @param null $id
+	 */
+    public function __construct($name, $id = null)
+    {
+		if($id) {
+			$this->id = $id;
+		}
+        $this->name = (string)$name;
+    }
+
+}
